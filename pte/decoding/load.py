80afe3c805313c6704a463db2b5ee4c6386c4471
Richard Koehler <richard.koehler@outlook.de>
2021-12-11 18:09:21 +0100

    Bugfix lineplot and loading

--- pte/decoding/load.py
+++ pte/decoding/load.py
@@ -2,6 +2,7 @@
 import json
 from pathlib import Path
 from typing import Iterable, Optional, Union
+import mne_bids
 
 import numpy as np
 import pte
@@ -13,13 +14,22 @@ def load_predictions_timelocked(
     baseline: Union[bool, tuple] = None,
     channels: Iterable = ("ECOG", "LFP"),
     keywords: Optional[Union[str, list]] = None,
-    average: Union[bool, int] = False,
+    key_average: Optional[str] = None,
 ):
-    """Load data from time-locked predictions"""
-    if baseline is not None:
+    """Load data from time-locked predictions."""
+    if not isinstance(fpath_or_dir, list):
+        file_finder = pte.get_filefinder(datatype="any")
+        file_finder.find_files(
+            directory=fpath_or_dir,
+            keywords=keywords,
+            extensions=["predictions_timelocked.json"],
+            verbose=True,
+        )
+        fpath_or_dir = file_finder.files
+    if baseline:
         if any(baseline) and not sfreq:
             raise ValueError(
-                "If `baseline` is any value other than `None` or `(None, None)`, `sfreq` must be provided."
+                "If `baseline` is any value other than `None`, `False` or `(None, None)`, `sfreq` must be provided."
             )
         if not sfreq:
             sfreq = 0.0
@@ -32,21 +42,17 @@ def load_predictions_timelocked(
         else:
             base_end = int(baseline[1] * sfreq)
 
-    if not isinstance(fpath_or_dir, list):
-        file_finder = pte.get_filefinder(datatype="any")
-        file_finder.find_files(
-            directory=fpath_or_dir,
-            keywords=keywords,
-            extensions=["predictions_timelocked.json"],
-            verbose=True,
-        )
-        fpath_or_dir = file_finder.files
-    data = []
-    for fpath in fpath_or_dir:
+    data = {ch_name: {} for ch_name in channels}
+    for key, fpath in enumerate(fpath_or_dir):
+        if key_average:
+            key = mne_bids.get_entities_from_fname(fpath, on_error="ignore")[
+                key_average
+            ]
         with open(fpath, "r", encoding="utf-8") as file:
             preds = json.load(file)
-        data_single = []
         for ch_name in channels:
+            if key not in data[ch_name]:
+                data[ch_name][key] = []
             pred = np.mean(np.stack(preds[ch_name], axis=0), axis=0)
             if baseline:
                 if baseline[1] is None:
@@ -54,11 +60,12 @@ def load_predictions_timelocked(
                 pred = (pred - np.mean(pred[base_start:base_end])) / (
                     np.std(pred[base_start:base_end])
                 )
-            data_single.append(pred)
-        data.append(data_single)
-    data = np.array(data)
-    if average:
-        if average is True:
-            average = 0
-        data = data.mean(axis=average)
-    return data
+            data[ch_name][key].append(pred)
+    data_outer = []
+    for k, value in data.items():
+        data_inner = []
+        for i, j in value.items():
+            data_inner.append(np.array(j).mean(axis=0))
+        data_outer.append(data_inner)
+    data_outer = np.array(data_outer)
+    return data_outer
--- pte/decoding/plot.py
+++ pte/decoding/plot.py
@@ -3,17 +3,17 @@ import os
 from pathlib import Path
 from typing import Iterable, Optional, Union
 
-import matplotlib
 import numpy as np
-from matplotlib import cm
+from matplotlib import axes, cm
 from matplotlib import pyplot as plt
+from statsmodels.stats.multitest import fdrcorrection
 
 import pte
 
 
 def lineplot_prediction(
-    ecog_data: np.ndarray,
-    lfp_data: np.ndarray,
+    x: np.ndarray,
+    y: np.ndarray,
     subpl_titles: Iterable,
     sfreq: Union[int, float],
     x_lims: tuple,
@@ -21,30 +21,75 @@ def lineplot_prediction(
     title: Optional[str] = None,
     subtitle: Optional[str] = None,
     label: str = "Distance from Hyperplane",
+    y_label: str = None,
     threshold: Union[int, float, tuple] = (0.0, 1.0),
     p_lim: float = 0.05,
     n_perm: int = 1000,
+    correction_method: str = "cluster",
+    two_tailed: bool = False,
     ylims=None,
+    compare_xy: bool = False,
 ) -> None:
-    """"""
+    """Plot averaged time-locked predictions including statistical tests."""
     viridis = cm.get_cmap("viridis", 8)
     colors = viridis(4), viridis(2)
 
-    fig, axs = plt.subplots(ncols=1, nrows=2, figsize=(7, 5), sharex=False)
+    nrows = 3 if compare_xy else 2
 
-    n_samples = ecog_data.shape[0]
+    fig, axs = plt.subplots(
+        ncols=1, nrows=nrows, figsize=(6, 2.0 * nrows), sharex=False
+    )
 
-    for i, data in enumerate((ecog_data, lfp_data)):
+    n_samples = x.shape[0]
+
+    for i, data in enumerate((x, y)):
         _single_lineplot(
             data=data,
             ax=axs[i],
             threshold=threshold,
             sfreq=sfreq,
+            x_lims=x_lims,
             color=colors[i],
             subpl_title=subpl_titles[i],
             label=label,
             p_lim=p_lim,
             n_perm=n_perm,
+            correction_method=correction_method,
+        )
+
+    if compare_xy:
+        for i, data in enumerate([x, y]):
+            label_ = (
+                " - ".join((subpl_titles[i], label))
+                if label
+                else subpl_titles[i]
+            )
+            axs[2].plot(
+                data.mean(axis=1), color=colors[i], label=label_,
+            )
+            axs[2].fill_between(
+                np.arange(data.shape[0]),
+                data.mean(axis=1) - data.std(axis=1),
+                data.mean(axis=1) + data.std(axis=1),
+                alpha=0.5,
+                color=colors[i],
+            )
+        axs[2].set_title(
+            f"{subpl_titles[0]} vs. {subpl_titles[1]}", fontsize="medium"
+        )
+        axs[2].set_xlabel("Time [s]")
+
+        p_vals = _get_p_vals(x=x, y=y, n_perm=n_perm, two_tailed=two_tailed)
+
+        _pval_correction_lineplot(
+            ax=axs[2],
+            x=x,
+            y=y,
+            x_lims=x_lims,
+            p_vals=p_vals,
+            p_lim=p_lim,
+            n_perm=n_perm,
+            correction_method=correction_method,
         )
 
     for ax in axs:
@@ -54,6 +99,7 @@ def lineplot_prediction(
         xticks = np.arange(0, n_samples, sfreq)
         ax.set_xticks(xticks)
         ax.set_xticklabels(np.linspace(x_lims[0], x_lims[1], len(xticks)))
+        ax.set_ylabel(y_label)
         ax.set_xlabel("Time (s)")
 
     if subtitle:
@@ -73,33 +119,86 @@ def lineplot_prediction(
     plt.show(block=True)
 
 
+def _pval_correction_lineplot(
+    ax: axes.Axes,
+    x: np.ndarray,
+    y: np.ndarray,
+    x_lims: tuple,
+    p_vals: Iterable,
+    p_lim: float,
+    correction_method: str,
+    n_perm: Optional[int] = None,
+) -> None:
+    """Perform p-value correction for singe lineplot."""
+    viridis = cm.get_cmap("viridis", 8)
+    if y.ndim == 1:
+        y = np.expand_dims(y, axis=1)
+
+    signif = np.where(p_vals <= p_lim)[0]
+    if signif.size > 0:
+        if correction_method == "cluster":
+            _, signif = pte.stats.clusterwise_pval_numba(
+                p_arr=p_vals, p_sig=p_lim, n_perm=n_perm
+            )
+        elif correction_method == "fdr":
+            rejected, _ = fdrcorrection(
+                pvals=p_vals, alpha=p_lim, method="poscorr", is_sorted=False
+            )
+            signif = [np.where(rejected)[0]]
+        else:
+            raise ValueError(
+                f"`correction_method` must be one of either `cluster` or `fdr`. Got:{correction_method}."
+            )
+        if signif:
+            x_labels = np.linspace(x_lims[0], x_lims[1], len(x)).round(2)
+            label = f"p-value <= {p_lim}"
+            for sig in signif:
+                lims = np.arange(sig[0], sig[-1] + 1)
+                y_lims = y.mean(axis=1)[lims]
+                ax.fill_between(
+                    lims,
+                    x.mean(axis=1)[lims],
+                    y_lims,
+                    alpha=0.5,
+                    color=viridis(7),
+                    label=label,
+                )
+                label = None  # Avoid printing label multiple times
+                for i in [0, -1]:
+                    x, y = lims[i], y_lims[i]
+                    ax.annotate(
+                        str(x_labels[x]) + "s",
+                        (x, y),
+                        xytext=(0.0, 15),
+                        textcoords="offset points",
+                        verticalalignment="center",
+                        horizontalalignment="center",
+                        arrowprops=dict(facecolor="black", arrowstyle="-"),
+                    )
+
+
 def _single_lineplot(
     data: np.ndarray,
-    ax: matplotlib.axes.Axes,
+    ax: axes.Axes,
     threshold: Union[Iterable, int, float],
     sfreq: Union[int, float],
+    x_lims: tuple,
     color: tuple,
     label: str,
-    subpl_title: Optional[str],
+    subpl_title: str,
     p_lim: float,
-    n_perm: Optional[int],
-):
+    n_perm: int,
+    correction_method: str,
+) -> None:
     """Plot prediction line for single model."""
-    viridis = cm.get_cmap("viridis", 8)
-    c_signif = viridis(7)
-    if isinstance(threshold, (int, float)):
-        threshold_value = threshold
-        threshold_arr = np.ones_like(data[:, 0]) * threshold_value
-    else:
-        threshold_value = data[
-            int(threshold[0] * sfreq) : int(threshold[1] * sfreq)
-        ].mean()
-        threshold_arr = np.ones_like(data[:, 0]) * threshold_value
-    p_vals = np.empty(data.shape[0])
-    for t_p, pred in enumerate(data):
-        _, p_vals[t_p] = pte.stats.permutation_numba_onesample(
-            x=pred, y=threshold_value, n_perm=n_perm, two_tailed=False
-        )
+    threshold_value, threshold_arr = _transform_threshold(
+        threshold=threshold, sfreq=sfreq, data=data
+    )
+
+    p_vals = _get_p_vals(
+        x=data, y=threshold_value, n_perm=n_perm, two_tailed=False
+    )
+
     ax.plot(data.mean(axis=1), color=color, label=label)
     ax.plot(
         threshold_arr,
@@ -114,35 +213,78 @@ def _single_lineplot(
         data.mean(axis=1) + data.std(axis=1),
         alpha=0.5,
         color=color,
+        label=None,
     )
-    signif = np.where(p_vals <= p_lim)[0]
-    if signif.size > 0:
-        _, signif = pte.stats.clusterwise_pval_numba(
-            p_arr=p_vals, p_sig=p_lim, n_perm=n_perm
-        )
-        if len(signif) == 0:
-            pass
-        elif len(signif) == 1:
-            signif = np.hstack(signif)
-            lims = np.arange(signif[0], signif[-1])
-            ax.fill_between(
-                lims,
-                data.mean(axis=1)[lims],
-                threshold_arr[lims],
-                alpha=0.5,
-                color=c_signif,
-                label=f"p-value <= {p_lim}",
-            )
-        else:
-            for i, sig in enumerate(signif):
-                lims = np.arange(sig[0], sig[-1])
-                label = f"p-value <= {p_lim}" if i == 0 else None
-                ax.fill_between(
-                    lims,
-                    data.mean(axis=1)[lims],
-                    threshold_arr(axis=1)[lims],
-                    alpha=0.5,
-                    color=c_signif,
-                    label=label,
-                )
+
+    _pval_correction_lineplot(
+        ax=ax,
+        x=data,
+        y=threshold_arr,
+        x_lims=x_lims,
+        p_vals=p_vals,
+        p_lim=p_lim,
+        n_perm=n_perm,
+        correction_method=correction_method,
+    )
+
     ax.set_title(subpl_title, fontsize="medium")
+
+
+def _get_p_vals(
+    x: Iterable, y: Union[Iterable, int, float], n_perm: int, two_tailed: bool
+):
+    """Calculate sample-wise p-values."""
+    p_vals = np.empty(len(x))
+    if isinstance(y, (int, float)):
+        for t_p, pred in enumerate(x):
+            _, p_vals[t_p] = pte.stats.permutation_onesample(
+                x=pred, y=y, n_perm=n_perm, two_tailed=two_tailed
+            )
+    else:
+        for i, (x_, y_) in enumerate(zip(x, y)):
+            _, p_vals[i] = pte.stats.permutation_twosample(
+                x=x_, y=y_, n_perm=n_perm, two_tailed=two_tailed
+            )
+    return p_vals
+
+
+def _transform_threshold(
+    threshold: Union[Iterable, int, float],
+    sfreq: Union[int, float],
+    data: np.ndarray,
+):
+    """Take threshold input and return threshold value and array."""
+    if isinstance(threshold, (int, float)):
+        threshold_value = threshold
+    else:
+        threshold_value = np.mean(
+            data[int(threshold[0] * sfreq) : int(threshold[1] * sfreq)]
+        )
+    threshold_arr = np.ones_like(data[:, 0]) * threshold_value
+    return threshold_value, threshold_arr
+
+
+def _add_median_labels(ax: axes.Axes) -> None:
+    """Add median labels to boxplot."""
+    lines = ax.get_lines()
+    # determine number of lines per box (this varies with/without fliers)
+    boxes = [c for c in ax.get_children() if type(c).__name__ == "PathPatch"]
+    lines_per_box = int(len(lines) / len(boxes))
+    # iterate over median lines
+    for median in lines[4 : len(lines) : lines_per_box]:
+        # display median value at center of median line
+        x, y = (data.mean() for data in median.get_data())
+        # choose value depending on horizontal or vertical plot orientation
+        value = (
+            x if (median.get_xdata()[1] - median.get_xdata()[0]) == 0 else y
+        )
+        _ = ax.text(
+            x=x,
+            y=y,
+            s=f"{value:.3f}",
+            ha="center",
+            va="center",
+            fontweight="light",
+            color="white",
+            fontsize="medium",
+        )
