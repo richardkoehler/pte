1e16daa7d43d55984b4e79ac4c98c47a6debd6cb
Richard Koehler <richard.koehler@outlook.de>
2021-12-14 10:22:56 +0100

    Add boxplot_performance

--- pte/decoding/plot.py
+++ pte/decoding/plot.py
@@ -1,14 +1,194 @@
 """Module for plotting decoding results."""
+from itertools import combinations, product
 import os
 from pathlib import Path
-from typing import Iterable, Optional, Union
+from typing import Callable, Iterable, Optional, Union
 
 import numpy as np
-from matplotlib import axes, cm
+import pandas as pd
+import seaborn as sns
+from matplotlib import axes, cm, collections, patheffects
 from matplotlib import pyplot as plt
+from statannotations.Annotator import Annotator
+from statannotations.stats.StatTest import StatTest
+
 from statsmodels.stats.multitest import fdrcorrection
 
 import pte
+
+
+def boxplot_performance(
+    data: pd.DataFrame,
+    outpath: Union[str, Path],
+    x: str,
+    y: str,
+    hue: Optional[str] = None,
+    order: Optional[Iterable] = None,
+    hue_order: Optional[Iterable] = None,
+    stat_test: Optional[str] = "Permutation",
+    add_lines: Optional[str] = None,
+    add_median_labels: bool = False,
+    title: Optional[str] = "Classification Performance",
+    figsize: Union[tuple, str] = "auto",
+) -> None:
+    """Plot performance as combined boxplot and stripplot."""
+    # data = data[["Channels", "Balanced Accuracy", "Subject"]]
+
+    color = "black"
+    alpha_box = 0.5
+
+    if not order:
+        order = data[x].unique()
+
+    if hue and not hue_order:
+        hue_order = data[hue].unique()
+
+    if figsize == "auto":
+        hue_factor = 1 if not hue else len(hue_order)
+        figsize = (1 * len(order) * hue_factor, 4)
+
+    plt.figure(figsize=figsize)
+
+    ax = sns.boxplot(
+        x=x,
+        y=y,
+        hue=hue,
+        order=order,
+        hue_order=hue_order,
+        data=data,
+        palette="viridis",
+        # showmeans=True,
+        meanline=False,
+        boxprops=dict(alpha=alpha_box),
+        showcaps=True,
+        showbox=True,
+        showfliers=False,
+        notch=False,
+        width=0.9,
+        whiskerprops={
+            "linewidth": 2,
+            "zorder": 10,
+            "alpha": alpha_box,
+            "color": color,
+        },
+        capprops={"alpha": alpha_box, "color": color},
+        medianprops=dict(
+            linestyle="-", linewidth=5, color=color, alpha=alpha_box
+        ),
+    )
+
+    if add_median_labels:
+        _add_median_labels(ax)
+
+    sns.stripplot(
+        x=x,
+        y=y,
+        hue=hue,
+        order=order,
+        hue_order=hue_order,
+        data=data,
+        palette="viridis",
+        dodge=True,
+        jitter=1.0,
+        s=7,
+        ax=ax,
+    )
+
+    if hue:
+        handles, labels = ax.get_legend_handles_labels()
+        new_labels = [
+            label.replace(" ", "\n") for label in labels[: len(labels) // 2]
+        ]
+        _ = plt.legend(
+            handles[: len(handles) // 2],
+            new_labels,
+            bbox_to_anchor=(1.02, 1),
+            loc=2,
+            borderaxespad=0.0,
+            title=hue,
+            labelspacing=0.7,
+        )
+
+    xlabels = [item.get_text() for item in ax.get_xticklabels()]
+    new_xlabels = [xtick.replace(" ", "\n") for xtick in xlabels]
+    ax.set_xticklabels(new_xlabels)
+    ax.set_title(title, fontsize="medium", y=1.02)
+
+    if add_lines:
+        _add_lines(
+            ax=ax, data=data, x=x, y=y, order=order, add_lines=add_lines
+        )
+
+    if stat_test:
+        _add_stats(ax, data, x, y, order, hue, hue_order, stat_test=stat_test)
+
+    plt.tight_layout()
+    plt.savefig(outpath, bbox_inches="tight", dpi=450)
+    plt.show(block=True)
+
+
+def _add_lines(
+    ax: axes.Axes,
+    data: pd.DataFrame,
+    x: str,
+    y: str,
+    order: list,
+    add_lines: str,
+):
+    """Add lines connecting single dots"""
+    data = data.sort_values(
+        by=x, key=lambda k: k.map({item: i for i, item in enumerate(order)})
+    )
+    lines = (
+        [[i, n] for i, n in enumerate(group)]
+        for _, group in data.groupby([add_lines], sort=False)[y]
+    )
+    lc = collections.LineCollection(lines, colors="grey", linewidths=1)
+    ax.add_collection(lc)
+
+
+def _add_stats(
+    ax: axes.Axes,
+    data: pd.DataFrame,
+    x: str,
+    y: str,
+    order: Iterable,
+    hue: Optional[str],
+    hue_order: Optional[Iterable],
+    stat_test: str,
+):
+    """Perform statistical test and annotate graph."""
+    if not hue:
+        pairs = list(combinations(order, 2))
+    else:
+        pairs = [
+            list(combinations(list(product([item], hue_order)), 2))
+            for item in order
+        ]
+        pairs = [item for sublist in pairs for item in sublist]
+
+    if stat_test == "Permutation":
+        stat_test = StatTest(
+            func=_permutation_wrapper,
+            n_perm=10000,
+            test_long_name="Permutation Test",
+            test_short_name="Perm.",
+            stat_name="Effect Size",
+        )
+    annotator = Annotator(
+        ax,
+        pairs,
+        data=data,
+        x=x,
+        y=y,
+        hue=hue,
+        hue_order=hue_order,
+        order=order,
+    )
+    annotator.configure(
+        test=stat_test, text_format="star", loc="inside", color="grey"
+    )
+    annotator.apply_and_annotate()
 
 
 def lineplot_prediction(
@@ -119,6 +299,51 @@ def lineplot_prediction(
     plt.show(block=True)
 
 
+def _permutation_wrapper(x, y, n_perm) -> Callable:
+    """Wrapper for statannotations to convert pandas series to numpy array."""
+    if isinstance(x, pd.Series):
+        x = x.values
+    if isinstance(y, pd.Series):
+        y = y.values
+    return pte.stats.permutation_twosample(x, y, n_perm)
+
+
+def _add_median_labels(ax: axes.Axes, add_borders: bool = False) -> None:
+    """"""
+    lines = ax.get_lines()
+    # determine number of lines per box (this varies with/without fliers)
+    boxes = [c for c in ax.get_children() if type(c).__name__ == "PathPatch"]
+    lines_per_box = int(len(lines) / len(boxes))
+    # iterate over median lines
+    for median in lines[4 : len(lines) : lines_per_box]:
+        # display median value at center of median line
+        x, y = (data.mean() for data in median.get_data())
+        # choose value depending on horizontal or vertical plot orientation
+        value = (
+            x if (median.get_xdata()[1] - median.get_xdata()[0]) == 0 else y
+        )
+        if add_borders:
+            # create median-colored border around white text for contrast
+            text = ax.text(
+                x,
+                y,
+                f"{value:.3f}",
+                ha="center",
+                va="center",
+                fontweight="ultralight",
+                color="white",
+                fontsize="medium",
+            )
+            text.set_path_effects(
+                [
+                    patheffects.Stroke(
+                        linewidth=0.0, foreground=median.get_color()
+                    ),
+                    patheffects.Normal(),
+                ]
+            )
+
+
 def _pval_correction_lineplot(
     ax: axes.Axes,
     x: np.ndarray,
@@ -147,7 +372,8 @@ def _pval_correction_lineplot(
             signif = [np.where(rejected)[0]]
         else:
             raise ValueError(
-                f"`correction_method` must be one of either `cluster` or `fdr`. Got:{correction_method}."
+                f"`correction_method` must be one of either `cluster` or `fdr`. \
+                Got:{correction_method}."
             )
         if signif:
             x_labels = np.linspace(x_lims[0], x_lims[1], len(x)).round(2)
@@ -165,10 +391,9 @@ def _pval_correction_lineplot(
                 )
                 label = None  # Avoid printing label multiple times
                 for i in [0, -1]:
-                    x, y = lims[i], y_lims[i]
                     ax.annotate(
-                        str(x_labels[x]) + "s",
-                        (x, y),
+                        str(x_labels[lims[i]]) + "s",
+                        (lims[i], y_lims[i]),
                         xytext=(0.0, 15),
                         textcoords="offset points",
                         verticalalignment="center",
