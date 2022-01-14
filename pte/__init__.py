11027dcdca534dd6f739cfea138b0c1841f92c29
Richard Koehler <richard.koehler@outlook.de>
2022-01-14 09:57:14 +0100

    Remove direct imports from subpackages

--- pte/__init__.py
+++ pte/__init__.py
@@ -1,10 +1,3 @@
 """Package for electrophysiological analyses."""
 
 from . import decoding, filetools, plotting, processing, stats, time_frequency
-from .filetools import (
-    add_coord_column,
-    get_bids_electrodes,
-    get_filefinder,
-    loadmat,
-    rewrite_bids_file,
-)
