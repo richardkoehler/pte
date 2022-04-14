342112020f73c7c746e61c6ce1ed2202b0f09b34
Richard Koehler <richard.koehler@outlook.de>
2022-04-14 06:56:19 +0200

    Add clusterplots

--- src/pte/plotting/__init__.py
+++ src/pte/plotting/__init__.py
@@ -1,6 +1,7 @@
 """Module for plotting functions."""
 
 from .coordinates import add_coords, find_structure_mni
+from .clusterplot import clusterplot_combined
 from .meshplot import meshplot_2d_compare
 from .normalization import scale_minmax
 from .raw import plotly_mne, plotly_rawdata
