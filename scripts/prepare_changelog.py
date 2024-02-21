6782e2730518401c6d3572243a4cce61aeb06f4a
Richard M. Köhler <richard.koehler@outlook.de>
2024-02-21 19:11:21 +0100

    Bump version to v0.2.0 for release

---
+++ scripts/prepare_changelog.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from pathlib import Path
+
+from pte_stats import __version__
+
+
+def main():
+    changelog = Path(r"docs/CHANGELOG.md")
+
+    with changelog.open() as f:
+        lines = f.readlines()
+
+    insert_index: int = -1
+    for i in range(len(lines)):
+        line = lines[i]
+        if line.startswith("## Unreleased"):
+            insert_index = i + 1
+        elif line.startswith(f"## [v{__version__}]"):
+            print("CHANGELOG already up-to-date")
+            return
+        elif line.startswith("## [v"):
+            break
+
+    if insert_index < 0:
+        raise RuntimeError("Couldn't find 'Unreleased' section")
+
+    lines.insert(insert_index, "\n")
+    lines.insert(
+        insert_index + 1,
+        f"## [v{__version__}]"
+        "(https://github.com/richardkoehler/pte-stats/releases"
+        f"/tag/v{__version__}) - "
+        f"{datetime.now().strftime('%Y-%m-%d')}\n",
+    )
+
+    with changelog.open("w") as f:
+        f.writelines(lines)
+
+
+if __name__ == "__main__":
+    main()
